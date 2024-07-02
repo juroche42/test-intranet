89efc00acc5d345b0006081f304dd1be770ad160
Jules Roche <julesro42@gmail.com>
2024-07-02 18:26:30 +0200

    create route put user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -13,5 +14,31 @@ class UserController extends Controller
             ->select(['id','prenom', 'nom', 'poste'])
             ->get();
         return response()->json($users);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'prenom' => 'required|string|max:255',
+            'nom' => 'required|string|max:255',
+            'poste' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $id,
+            'statut' => 'required|int|in:0,1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->update($request->all());
+
+        return response()->json($user, 200);
     }
 }
