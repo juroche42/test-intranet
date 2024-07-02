42cb8c39e9d71222297a3060b05a0a7376570df0
Jules Roche <julesro42@gmail.com>
2024-07-02 18:23:15 +0200

    create Route getAllUser()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::with('services:name')
+            ->select(['id','prenom', 'nom', 'poste'])
+            ->get();
+        return response()->json($users);
+    }
 }
