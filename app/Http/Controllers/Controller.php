580d22d0a108bd15b9c7c13da17aa5696f013d60
Jules Roche <julesro42@gmail.com>
2024-07-03 13:11:45 +0200

    adding OA info

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Laravel API",
+ *     version="1.0.0",
+ *     description="This is a sample Laravel API documentation",
+ * )
+ * @OA\Server(
+ *    url="http://localhost:8000",
+ *   description="Development server"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
