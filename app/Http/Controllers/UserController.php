0adc16cf4f16b1b8c2036428f0e0c402a166ad46
Jules Roche <julesro42@gmail.com>
2024-07-03 11:47:59 +0200

    documentation endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use OpenApi\Annotations as OA;
 
 class UserController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *     path="/users",
+     *     tags={"Users"},
+     *     summary="Get all users",
+     *     description="Get all users",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/User")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     )
+     * )
+     */
     public function index()
     {
         $users = User::with('services:name')
@@ -17,6 +43,35 @@ class UserController extends Controller
     }
 
 
+    /**
+     * @OA\Post(
+     *     path="/users",
+     *     tags={"Users"},
+     *     summary="Create a user",
+     *     description="Create a user",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     )
+     * )
+     */
     public function update(Request $request, int $userId)
     {
         $validator = Validator::make($request->all(), [
@@ -42,6 +97,44 @@ class UserController extends Controller
         return response()->json($user, 200);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/users/{userId}",
+     *     tags={"Users"},
+     *     summary="Delete a user",
+     *     description="Delete a user",
+     *     @OA\Parameter(
+     *         name="userId",
+     *         in="path",
+     *         description="ID of user to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="User not found"
+     *     )
+     * )
+     */
     public function delete(int $userId)
     {
         $user = User::find($userId);
